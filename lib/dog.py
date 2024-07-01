4347508677df43a53fb5e6ea86162beed573e854
Mohammad Khan <qcc2016@yahoo.com>
2024-07-01 09:52:29 -0400

    added functions

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
+# class Dog:
+#     def __init__(self, name):
+#         self.name = name  
+
+#     def bark(self):
+#         print("Woof!")
+#         return "Woof!"  
 
 class Dog:
-    # Class body goes here
+    def bark(self):
+        print("Woof!")
 
-    #Instance method definition
-    pass
+    def sit(self):
+        print("The dog is sitting.")
